11a07dba9513baed17f2e13602fb4c98e09860b5
iago <iago.pferreiravr@gmail.com>
2021-09-01 19:39:22 -0300

    Ex02

--- exercises/bloco_26/dia_4/exercicios/index.js
+++ exercises/bloco_26/dia_4/exercicios/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/ping', (_req, res) => res.json({ message: 'pong' }));
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ message: `Hello, ${name}!`})
+});
 
 app.listen(3000, () => console.log('O pai ta on na 3000'))
