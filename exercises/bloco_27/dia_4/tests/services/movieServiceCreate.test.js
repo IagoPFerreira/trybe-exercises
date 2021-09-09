ba5a00c2f9547afed1bce4308dbd0d98e782160b
iago <iago.pferreiravr@gmail.com>
2021-09-09 16:38:50 -0300

    Implementando os testes do service do movieCreate

---
+++ exercises/bloco_27/dia_4/tests/services/movieServiceCreate.test.js
@@ -0,0 +1,37 @@
+const { expect } = require('chai');
+
+const MoviesService = {
+  create: () => {},
+};
+
+describe('Insere um novo filme no BD', () => {
+  describe('quando o payload informado não é válido', () => {
+    const payloadMovie = {};
+
+    it('retorna um boolean', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.a('boolean');
+    });
+  });
+
+  describe('quando é inserido com sucesso', () => {
+    const payloadMovie = {
+      title: 'Example Movie',
+      directedBy: 'Jane Dow',
+      releaseYear: 1999,
+    };
+
+    it('retorna um objeto', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.a('object');
+    });
+
+    it('tal objeto possui o "id" do novo filme inserido', async () => {
+      const response = await MoviesService.create(payloadMovie);
+
+      expect(response).to.be.a.property('id');
+    });
+  });
+});
